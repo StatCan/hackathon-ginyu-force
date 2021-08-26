5104680f9f6c3343b4a13ce8fec8f780b5250231
Blair Drummond <blair.drummond@canada.ca>
2021-08-25 21:25:37 -0400

    feat: add substitution script and svgs

---
+++ .github/scripts/substitute.py
@@ -0,0 +1,51 @@
+#!/bin/python3
+
+import os
+import json
+from argparse import ArgumentParser
+from typing import Dict
+import jinja2
+from jinja2 import Template, FileSystemLoader, StrictUndefined
+
+jinja_env = jinja2.Environment(
+    loader=FileSystemLoader('.'),
+    undefined=StrictUndefined
+)
+
+ROOT = 'docs'
+
+def templatify(dest: str, values: Dict):
+    try:
+        template = jinja_env.get_template(dest)
+    except UnicodeDecodeError:
+        return
+
+    s = template.render(**values)
+    if PRINT_ONLY:
+        print(s)
+    else:
+        with open(dest, 'w') as f:
+            f.write(s)
+
+def traverse(values):
+    # Copy and template everything!
+    for (dirpath, dirnames, filenames) in os.walk(ROOT):
+        for filename in filenames:
+            fullname = os.sep.join([dirpath, filename])
+            templatify(fullname, values)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description='Template variables with jinja')
+    parser.add_argument(
+        'file',
+        type=str,
+        help='A .json file to read substitutions from'
+       )
+
+    # Get json data to subsititute
+    args = parser.parse_args()
+    with open(args.file) as f:
+        values = json.load(f)
+
+    traverse(values)
--- docs/index.md
+++ docs/index.md
@@ -1,4 +1,8 @@
 # Title
 
-Text
+<img
+    src="content/infographic_estma_reports.svg"
+    alt="{{ estma_reports_count }} ESTMA Reports"
+    height="100"
+    width="200" />
 
