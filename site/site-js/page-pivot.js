931f2a6066ffe5f6308ea11e7fa1050098f04d85
Brendan Gadd <brendan.gadd@canada.ca>
2021-08-27 12:36:36 -0400

    Save pivot state in URL

--- site/site-js/page-pivot.js
+++ site/site-js/page-pivot.js
@@ -1,12 +1,25 @@
 $(function () {
   const DATA_URL =
     "https://raw.githubusercontent.com/StatCan/hackathon-ginyu-force-data/main/SAMPLE-ESTMA-data.csv";
-  const renderers = $.extend(
-    $.pivotUtilities.renderers,
-    $.pivotUtilities.plotly_renderers,
-    $.pivotUtilities.d3_renderers,
-    $.pivotUtilities.export_renderers
-  );
+  const STATE_KEYS = [
+    "cols",
+    "rows",
+    "vals",
+    "rowOrder",
+    "colOrder",
+    "exclusions",
+    "inclusions",
+    //"inclusionsInfo",
+    "aggregatorName",
+    "rendererName",
+  ];
+  const DEFAULT_STATE = {
+    aggregatorName: "Sum",
+    vals: ["amount_reported_cad"],
+    cols: ["reporting_cylce"],
+    rows: ["jurisdiction"],
+  };
+
   fetch(DATA_URL)
     .then((res) => {
       if (!res.ok) {
@@ -16,7 +29,7 @@ $(function () {
     })
     .then(parseCsv)
     .then((parsed) => initPivotTable(parsed.data))
-    .catch((e) => alert(`Failed to load table from:\n${DATA_URL}`));
+    .catch((_) => alert(`Failed to load table from:\n${DATA_URL}`));
 
   function parseCsv(csv) {
     return new Promise(function (resolve, reject) {
@@ -29,6 +42,25 @@ $(function () {
   }
 
   function initPivotTable(data) {
+    const renderers = $.extend(
+      $.pivotUtilities.renderers,
+      $.pivotUtilities.plotly_renderers,
+      $.pivotUtilities.d3_renderers,
+      $.pivotUtilities.export_renderers
+    );
+
+    try {
+      var hashState = _(getNormalizedHash())
+        .thru((hash) => new URLSearchParams(hash).get("state"))
+        .thru(atob)
+        .thru(JSON.parse)
+        .pick(STATE_KEYS)
+        .value();
+    } catch {
+      hashState = {};
+    }
+    const state = _.defaults(hashState, DEFAULT_STATE);
+
     const iEntity = data[0].indexOf("entity");
     const uniques = data.slice(1).reduce(function (memo, row) {
       memo[row[iEntity]] = true;
@@ -37,10 +69,7 @@ $(function () {
     $("#table").pivotUI(
       data,
       {
-        aggregatorName: "Sum",
-        vals: ["amount_reported_cad"],
-        cols: ["reporting_cylce"],
-        rows: ["jurisdiction"],
+        ...state,
         hiddenAttributes: [
           "estma_id",
           "payment_category",
@@ -50,10 +79,28 @@ $(function () {
           "payment_notes",
         ],
         hiddenFromDragDrop: ["amount_reported_cad"],
+        autoSortUnusedAttrs: true,
         menuLimit: Object.keys(uniques).length,
         renderers: renderers,
+        onRefresh: onTableRefresh,
       },
       true
     );
+  }
+
+  function onTableRefresh(config) {
+    const state = _(config)
+      .pick(STATE_KEYS)
+      .thru(JSON.stringify)
+      .thru(btoa)
+      .value();
+    const params = new URLSearchParams(getNormalizedHash());
+    params.set("state", state);
+    window.location.hash = params.toString();
+  }
+
+  function getNormalizedHash() {
+    const hash = window.location.hash;
+    return hash.length > 0 ? hash.substr(1) : hash;
   }
 });
