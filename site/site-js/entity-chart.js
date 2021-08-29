df9a720a9d6c1aef8cc65b0ecf4e112638473a94
Brendan Gadd <brendan.gadd@canada.ca>
2021-08-29 13:21:50 -0400

    Switch data frame library to Arquero
    Switches to Arquero to boost data frame performance by an order of
    magnitude. Also switches to module-style for the charting script.
    
    Implementation still uses a web worker, which is provided out of the box
    by the library.

--- site/site-js/entity-chart.js
+++ site/site-js/entity-chart.js
@@ -1,126 +1,154 @@
-(async function () {
-  const DataFrame = dfjs.DataFrame;
+import {
+  op,
+  worker,
+} from "https://cdn.jsdelivr.net/npm/arquero-worker@0.0.2/dist/arquero-worker-client.mjs";
 
-  const DATA_URL =
-    "https://raw.githubusercontent.com/StatCan/hackathon-ginyu-force-data/main/SAMPLE-ESTMA-data.csv";
-  const cols = {
-    entity: "entity",
-    cycle: "reporting_cylce",
-    amount: "amount_reported_cad",
-  };
+import "https://cdn.jsdelivr.net/npm/jquery@2.2.4";
+import "https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0";
 
-  let dataFrame = null,
-    allEntitiesFrame = null,
-    chart = null;
+//const op = aq.op;
 
-  document.addEventListener("DOMContentLoaded", main);
+const DATA_URL =
+  "https://raw.githubusercontent.com/StatCan/hackathon-ginyu-force-data/main/SAMPLE-ESTMA-data.csv";
+const cols = {
+  entity: "entity",
+  cycle: "reporting_cylce",
+  amount: "amount_reported_cad",
+};
 
-  async function main() {
-    const loc = window.location;
-    // TODO: Improve this hacky business.
-    // If on GitHub pages, assume site is deployed at root of repo path.
-    // Note: GitHub pages root is <account>.github.io/<repo_name>/
-    let workerUrl = loc.host.endsWith(".github.io")
-      ? loc.pathname.match(/\/[^/]*/)[0]
-      : "";
-    workerUrl += "/site-js/data-frame-worker.js";
-    const dfWorker = new Worker(workerUrl);
-    dfWorker.onmessage = onDataFrameLoaded;
-    dfWorker.postMessage(DATA_URL);
+let chart = null;
+
+document.addEventListener("DOMContentLoaded", main);
+
+async function main() {
+  const loc = window.location;
+  // TODO: Improve this hacky business.
+  // If on GitHub pages, assume site is deployed at root of repo path.
+  // Note: GitHub pages root is <account>.github.io/<repo_name>/
+  let workerUrl = loc.host.endsWith(".github.io")
+    ? loc.pathname.match(/\/[^/]*/)[0]
+    : "";
+  workerUrl += "/site-js/arquero-worker.min.js";
+  const aqWorker = worker(workerUrl);
+
+  const rootDf = await aqWorker.load("payments", DATA_URL);
+
+  let stop = timer("Worker frame");
+  const perEntityDf = await rootDf
+    .select({
+      [cols.entity]: "entity",
+      [cols.cycle]: "cycle",
+      [cols.amount]: "amount",
+    })
+    .derive({
+      cycle: (d) => "" + d.cycle,
+      amount: (d) => op.parse_float(op.replace(d.amount, /,/g, "")),
+    })
+    .groupby("entity", "cycle")
+    .rollup({ amount: op.sum("amount") })
+    .orderby("cycle")
+    .fetch();
+  stop();
+
+  stop = timer("Foreground frames");
+  const allEntityData = perEntityDf
+    .groupby("cycle")
+    .rollup({ amount: op.sum("amount") })
+    .objects();
+  const entities = perEntityDf
+    .select("entity")
+    .dedupe()
+    .orderby("entity")
+    .reify()
+    .column("entity").data;
+  stop();
+
+  document.getElementById("chart-spinner").style.display = "none";
+  document.getElementById("chart-view").style.display = "block";
+
+  initEntityDropdown(function (e) {
+    const value = e.target.value;
+    const opts =
+      value == "__ALL__"
+        ? {
+            entity: "(All Entities)",
+            data: allEntityData,
+          }
+        : {
+            entity: value,
+            data: perEntityDf
+              .params({ entity: value })
+              .filter((d, $) => d.entity == $.entity)
+              .objects(),
+          };
+    renderChart(opts);
+  }, entities);
+
+  renderChart({ entity: "(All Entities)", data: allEntityData });
+}
+
+function initEntityDropdown(onEntitySelect, entities) {
+  const entityDropdown = document.querySelector("#entities");
+
+  const frag = document.createDocumentFragment();
+  frag.appendChild(createOption("__ALL__", "(All)"));
+  entities
+    .map((entity) => createOption(entity, entity))
+    .forEach((option) => frag.appendChild(option));
+  entityDropdown.appendChild(frag);
+
+  $(entityDropdown).select2().on("change", onEntitySelect);
+}
+
+function createOption(value, text) {
+  const option = document.createElement("option");
+  option.value = value;
+  option.textContent = text;
+  return option;
+}
+
+function renderChart({ entity, data }) {
+  const ctx = document.getElementById("chart").getContext("2d");
+  if (chart) {
+    chart.destroy();
   }
-
-  function onDataFrameLoaded(e) {
-    const { data, columns } = e.data;
-    e.target.terminate();
-
-    dataFrame = new DataFrame(data, columns);
-    allEntitiesFrame = dataFrame
-      .groupBy(cols.cycle)
-      .aggregate((group) => group.stat.sum(cols.amount))
-      .rename("aggregation", cols.amount);
-
-    const entities = dataFrame.distinct(cols.entity).toArray().flat().sort();
-
-    document.getElementById("chart-spinner").style.display = "none";
-    document.getElementById("chart-view").style.display = "block";
-
-    initEntityDropdown(entities);
-    renderChart("__ALL__");
-  }
-
-  function initEntityDropdown(entities) {
-    const entityDropdown = document.querySelector("#entities");
-
-    const frag = document.createDocumentFragment();
-    frag.appendChild(createOption("__ALL__", "(All)"));
-    entities
-      .map((entity) => createOption(entity, entity))
-      .forEach((option) => frag.appendChild(option));
-    entityDropdown.appendChild(frag);
-
-    $(entityDropdown).select2().on("change", onEntitySelect);
-  }
-
-  function createOption(value, text) {
-    const option = document.createElement("option");
-    option.value = value;
-    option.textContent = text;
-    return option;
-  }
-
-  function onEntitySelect(e) {
-    const selectedEntity = e.target.value;
-    if (selectedEntity == "") {
-      return;
-    }
-    renderChart(selectedEntity);
-  }
-
-  function renderChart(entity) {
-    const data = (
-      entity === "__ALL__"
-        ? allEntitiesFrame
-        : dataFrame.where((row) => row.get(cols.entity) == entity)
-    ).toCollection();
-
-    const entityName = entity == "__ALL__" ? "(All)" : entity;
-    const ctx = document.getElementById("chart").getContext("2d");
-    if (chart) {
-      chart.destroy();
-    }
-    chart = new Chart(ctx, {
-      type: "bar",
-      data: {
-        datasets: [
-          {
-            data,
-            label: entityName,
-            backgroundColor: "#00cc96",
-          },
-        ],
-      },
-      options: {
-        parsing: {
-          xAxisKey: cols.cycle,
-          yAxisKey: cols.amount,
+  chart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      datasets: [
+        {
+          data,
+          label: entity,
+          backgroundColor: "#00cc96",
         },
-        scales: {
-          x: { title: { display: true, text: "Reporting Cycle" } },
-          y: {
-            title: { display: true, text: "Amount Reported ($)" },
-            ticks: {
-              callback: function (value, index, values) {
-                return "$" + parseInt(value).toLocaleString();
-              },
+      ],
+    },
+    options: {
+      parsing: {
+        xAxisKey: "cycle",
+        yAxisKey: "amount",
+      },
+      scales: {
+        x: { title: { display: true, text: "Reporting Cycle" } },
+        y: {
+          title: { display: true, text: "Amount Reported ($)" },
+          ticks: {
+            callback: function (value, index, values) {
+              return "$" + parseInt(value).toLocaleString();
             },
           },
         },
-        plugins: {
-          legend: {
-            display: false,
-          },
+      },
+      plugins: {
+        legend: {
+          display: false,
         },
       },
-    });
-  }
-})();
+    },
+  });
+}
+
+function timer(label) {
+  const t0 = performance.now();
+  return () => console.log(label, performance.now() - t0);
+}
